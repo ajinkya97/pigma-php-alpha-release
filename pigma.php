cee08c5d02aa045f771e8b3e9ab40efc6aef303b
karthik koppaka <34645468+officialk@users.noreply.github.com>
2018-05-29 17:42:55 +0530

    updated pigma
    included FileHandling class

--- pigma.php
+++ pigma.php
@@ -1,5 +1,6 @@
 <?php
-include("DeveloperUtils.php");
-include("Utils.php");
-include("Database.php");
+    include("DeveloperUtils.php");//checked and working
+    include("Utils.php");//checked and working
+    include("Database.php");//checked and working
+    include("FileHandling.php");//checked and working
 ?>
