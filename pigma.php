b82559ae29d997a35a968f6a15b3c216054b48fc
karthik koppaka <34645468+officialk@users.noreply.github.com>
2018-05-31 18:13:22 +0530

    updated the dependencies
    updated class files to be require_once or include_once

--- pigma.php
+++ pigma.php
@@ -1,8 +1,8 @@
 <?php
-    include("DeveloperUtils.php");//checked and working
-    include("Utils.php");//checked and working
-    include("Database.php");//checked and working
-    include("FileHandling.php");//checked and working
-    include("html.php");//under Development
-    include("Cryptography.php");//has bugs
+    require_once("DeveloperUtils.php");//checked and working
+    include_once("Utils.php");//checked and working
+    include_once("Database.php");//checked and working
+    include_once("FileHandling.php");//checked and working
+    include_once("html.php");//under Development
+    include_once("Cryptography.php");//has bugs
 ?>
