a421c4260bf16b009cd51840af26fd1edef3c5d2
karthik koppaka <34645468+officialk@users.noreply.github.com>
2018-06-06 01:01:26 +0530

    updated Database.php
    cleared/fixed major bugs in insert,update and delete
    kick started the select function
    added the DeveloperUtils file[require once] so that the user can individually import/include needed files

--- Database.php
+++ Database.php
@@ -1,4 +1,5 @@
 <?php
+require_once("DeveloperUtils.php");
 class Database{
     public $hostname = "";
     public $username = "";
@@ -32,7 +33,7 @@ class Database{
                     return $ret;
                 }
             }
-            elseif(phpversion()>7){
+            elseif(phpversion()>=7){
                 $this->con=mysqli_connect($this->hostname,$this->username,$this->password,$this->dbname) or die(mysqli_error($this->con));
                 if(!$this->con){
                     return $this->databaseNotConnected;
@@ -57,7 +58,7 @@ class Database{
                     return $ret;
                 }
             }
-            elseif(phpversion()>7){
+            elseif(phpversion()>=7){
                 $ret = mysqli_query($this->con,$query) or die("Error".mysqli_error($this->con)."<br>query:".$query);
                 if(!$ret){
                     return $this->queryFailed;
@@ -81,7 +82,7 @@ class Database{
             if(phpversion()<7){
                 $ret = mysql_fetch_array($data);
             }
-            else if(phpversion()>7){
+            else if(phpversion()>=7){
                 $ret = mysqli_fetch_array($data);
             }
             return $ret;
@@ -93,7 +94,7 @@ class Database{
             if(phpversion()<7){
                 $ret = mysql_fetch_assoc($data);
             }
-            else if(phpversion()>7){
+            else if(phpversion()>=7){
                 $ret = mysqli_fetch_assoc($data);
             }
             return $ret;
@@ -111,7 +112,7 @@ class Database{
             $table = $args[0];
             $query = "";
             //insert into "tablename" values(val1,val2,val3...)
-            if(count($args)==2 or (count($args)==3 and is_bool($args[2]))){
+            if(count($args)==2 or (count($args)==3 and is_bool($args[2]) and $args[2]==true)){
                 if(is_array($args[1])){
                     $val = makeString($args[1]);    
                 }
@@ -121,20 +122,10 @@ class Database{
                 $query = "insert into $table values ($val);";
             }
             //insert into "tablename"(col1,col2,col3...) values(val1,val2,val3...)
-            elseif(count($args)==3 or (count($args)==4 and is_bool($args[3]))){
+            elseif(count($args)==3 or (count($args)==4 and is_bool($args[3]) and $args[3]==true)){
                 if(is_string($args[1]) and is_string($args[2])){
                     $col = makeString(explode(",",$args[1]),',','');
                     $val = makeString(explode(",",$args[2]));
-                    $query = "insert into $table($col) values ($val);";
-                }
-                elseif(is_string($args[1]) and is_array($args[2])){
-                    $col = makeString(explode(',',$args[1]),',','');
-                    $val = makeString($args[2]);
-                    $query = "insert into $table($col) values ($val);";
-                }
-                elseif(is_array($args[1]) and is_string($args[2])){
-                    $col = makeString($args[1],',','');
-                    $val = makeString(explode(',',$args[2]));
                     $query = "insert into $table($col) values ($val);";
                 }
                 elseif(is_array($args[1]) and is_array($args[2])){
@@ -148,7 +139,7 @@ class Database{
                     $col = '';
                     $val = '';
                     if(count($args)>1){
-                        if($args[count($args)-1]==true){
+                        if(is_bool($args[count($args)-1]) and $args[count($args)-1]==true){
                             for ($j = 1; $j < count($args)-1; $j++){
                                 if (($j % 2) == 0) {$val = $val . "'" . $args[$j] . "',";}
                                 if (($j % 2) == 1) {$col = $col . "`" . $args[$j] . "`,";}
@@ -192,29 +183,21 @@ class Database{
                 $changes = $args[1];
                 $query = "update $table set $changes";
             }
-            else if($cnt==3 or ($cnt==4 and $args[3]==true)){
+            else if($cnt==3 or ($cnt==4 and is_bool($args[3]) and $args[3]==true)){
                 $changes = "";
                 if(is_string($args[1]) and is_string($args[2])){
-                    $val = makeString(explode(',',$args[1]));
-                    $col = makeString(explode(',',$args[2]),',','');
-                }
-                else if(is_array($args[1]) and is_string($args[2])){
-                    $val = $args[1];
-                    $col = makeString(explode(',',$args[2]),',','');
-                }
-                else if(is_string($args[1]) and is_array($args[2])){
-                    $val = makeString(explode(',',$args[1]));
-                    $col = $args[2];
+                    $val = explode(',',$args[2]);
+                    $col = explode(',',$args[1]);
                 }
                 else if(is_array($args[1]) and is_array($args[2])){
-                    $val = $args[1];
-                    $col = $args[2];
+                    $val = $args[2];
+                    $col = $args[1];
                 }
                 else{
                     return $this->error("Invalid Parameters");
                 }
                 for($i=0;$i<count($val);$i++){
-                    $changes .= "$col[$i]=$val[$i],";
+                    $changes .= "$col[$i]='$val[$i]',";
                 }
                 $changes = rtrim($changes,",");
                 $query = "update $table set $changes";
@@ -225,12 +208,12 @@ class Database{
                     $where = $args[3];
                 }
                 elseif(is_array($args[3])){
-                    $where = makeString($args[3],',','');
+                    $where = makeString($args[3],' and ','');
                 }
                 else{
                     $this->error("Invalid Parameters");
                 }
-                $query = $this->update($table,$args[1],$args[2],true)." $where";
+                $query = $this->update($table,$args[1],$args[2],true)." where $where";
             }
             else{
                 return $this->error("invalid parameters");
@@ -249,15 +232,18 @@ class Database{
             $args = func_get_args();
             $table = $args[0];
             $cnt = count($args);
-            if($cnt==1 or ($cnt==2 and $args[1]==true)){
+            if($cnt==1 or ($cnt==2 and is_bool($args[1]) and $args[1]==true)){
                 $query = "delete from $table";
             }
-            else if($cnt==2 or ($cnt==3 and $args[2]==true)){
+            else if($cnt==2 or ($cnt==3 and is_bool($args[2]) and $args[2]==true)){
                 $where = $args[1];
                 if(is_array($args[1])){
                     $where = makeString($args[1],' and ','');
                 }
-                $query = $this->update($table,true)." $where";
+                elseif(is_string($args[1])){
+                    $where = $args[1];
+                }
+                $query = $this->delete($table,true)." where $where";
             }
             else{
                 $this->error("Invalid parameters");
@@ -272,8 +258,44 @@ class Database{
         //select functions
         function select(){
             $args = func_get_args();
-            $query = $args[0];
-            return $this->query($query);
+            $table = $args[0];
+            $cnt = count($args);
+            if($cnt==1 or ($cnt==2 and is_bool($args[1]) and $args[1]==true)){
+                $query = "select * from $table";
+            }
+            else if($cnt==2 or ($cnt==3 and is_bool($args[2]) and $args[2]==true)){
+                $cols = "";
+                if(is_array($args[1])){
+                    $cols = makeString($args[1],',','');
+                }
+                elseif(is_string($args[1])){
+                    $cols = $args[1];
+                }
+                $query = "select $cols from $table";
+            }
+            else if($cnt==3 or ($cnt==4 and is_bool($args[3]) and $args[3]==true)){
+                $where = "";
+                if(is_array($args[2])){
+                    $where = makeString($args[2],' and ','');
+                }
+                elseif(is_string($args[2])){
+                    $where = $args[2];
+                }
+                $query = $this->select($table,$args[1],true)." where $where";
+            }
+            else if($cnt==4 or ($cnt==5 and is_bool($args[4]) and $args[4]==true)){
+                $other = $args[3];
+                $query = $this->select($table,$args[1],$args[2],true)." $other";
+            }
+            else{
+                $this->error("Invalid parameters");
+            }
+            if(is_bool($args[count($args)-1]) and $args[count($args)-1]==true){
+                return $query;
+            }
+            else{
+                return $this->query($query);
+            }
         }
     //}
 //    //table functions
@@ -294,7 +316,8 @@ class Database{
             if(count($args)==2){
                 $type = $args[0];
                 $name = $args[1];
-                if(is_string($type) and is_array($name)){
+                if(is_array($type) and is_array($name)){
+                    $type = makeString($type,',','');
                     $name = makeString($name,',','');
                 }
                 $query = "drop $type $name;";
